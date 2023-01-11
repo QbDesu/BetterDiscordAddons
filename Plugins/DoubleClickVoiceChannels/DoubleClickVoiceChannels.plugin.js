45a9244fe2929f2e296cac2fa2a5023097d46ce6
Qb <qb@qbit.moe>
2023-01-11 21:52:38 +0100

    DoubleClickVoiceChannels: 1.1.0 update

--- Plugins/DoubleClickVoiceChannels/DoubleClickVoiceChannels.plugin.js
+++ Plugins/DoubleClickVoiceChannels/DoubleClickVoiceChannels.plugin.js
@@ -1,9 +1,9 @@
 /**
 * @name DoubleClickVoiceChannels
 * @description Requires you to double click voice channels to join them.
-* @author Qb, Twizzer#0001 & AAGaming#9395
+* @author Qb
 * @authorId 133659541198864384
-* @version 1.0.0
+* @version 1.1.0
 * @invite pKx8m6Z
 * @license Unlicensed
 * @source https://github.com/QbDesu/BetterDiscordAddons/blob/potato/Plugins/DoubleClickVoiceChannels
@@ -12,34 +12,24 @@
 /*@cc_on
 @if (@_jscript)
 
-// Offer to self-install for clueless users that try to run this directly.
 var shell = WScript.CreateObject("WScript.Shell");
-var fs = new ActiveXObject("Scripting.FileSystemObject");
-var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
-var pathSelf = WScript.ScriptFullName;
-// Put the user at ease by addressing them in the first person
-shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-    shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-} else if (!fs.FolderExists(pathPlugins)) {
-    shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-    fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-    // Show the user where to put plugins in the future
-    shell.Exec("explorer " + pathPlugins);
-    shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-}
-WScript.Quit();
+shell.Popup("It looks like you've mistakenly tried to run me directly. That's not how you install plugins. \n(So don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
 
 @else@*/
-
 module.exports = (() => {
     const config = {
         info: {
             name: "DoubleClickVoiceChannels",
-            version: "1.0.0",
+            version: "1.1.0",
             github_raw: "https://raw.githubusercontent.com/QbDesu/BetterDiscordAddons/potato/Plugins/DoubleClickVoiceChannels/DoubleClickVoiceChannels.plugin.js"
-        }
+        },
+        changelog: [
+            {
+                title: 'Fixes',
+                type: 'fixed',
+                items: ['Finally got around to fixing the plugin after the big update a couple months ago... Sorry for the wait.'],
+            },
+        ],
     };
     return !global.ZeresPluginLibrary ? class {
         constructor() { this._config = config; }
@@ -62,107 +52,71 @@ module.exports = (() => {
         start() { }
         stop() { }
     }
-: (([Plugin, Api]) => {
-    const plugin = (Plugin, Api) => {
-		const {
-            Patcher,
-            WebpackModules,
-            Logger
-		} = Api;
+        : (([Plugin, Api]) => {
+            const plugin = (Plugin, Api) => {
+                const {
+                    WebpackModules,
+                    Logger
+                } = Api;
+                const {
+                    Patcher,
+                    Webpack: { Filters, getModule },
+                    Utils: { findInTree }
+                } = new BdApi(config.info.name);
 
-        const ChannelItem = WebpackModules.getModule(m => m?.default?.displayName == "ChannelItem");
-        const ChannelMention = WebpackModules.getModule(m => m?.default?.displayName == "Mention");
 
-        return class GuildNotificationDefaults extends Plugin {
-            constructor() {
-                super();
-                // remove need for duplication of metadata fields
-                this.getAuthor = null;
-                this.getDescription = null;
-            }
-            
-            async onStart() {
-                Patcher.after(ChannelItem, "default", (that, args, value) => {
-                    const channel = this.getChannel(value);
-                    if (channel) {
-                        if (channel.type === 2 || channel.type === 13) {
-                            const clickable = this.getClickable(value);
+                const getModuleAndKey = (filter) => {
+                    let module;
+                    const value = getModule((e, m) => filter(e) ? (module = m) : false, { searchExports: true });
+                    if (!module) return;
+                    return [module.exports, Object.keys(module.exports).find(k => module.exports[k] === value)];
+                };
+
+
+                const ChannelItem = getModuleAndKey(Filters.byStrings("canHaveDot", "unreadRelevant", "UNREAD_HIGHLIGHT"));
+                const ChatMessage = getModuleAndKey(Filters.byStrings("voice-locked", "VOICE_CHANNEL_LOCKED"));
+
+                return class GuildNotificationDefaults extends Plugin {
+                    constructor() {
+                        super();
+                        // remove need for duplication of metadata fields
+                        this.getAuthor = null;
+                        this.getDescription = null;
+                    }
+
+                    async onStart() {
+                        Patcher.after(...ChannelItem, (_this, [{ channel }], value) => {
+                            if (channel?.type === 2 || channel?.type === 13) {
+                                const clickable = findInTree(value, (item) => item?.onClick && item.role === "button", { walkable: ["props", "children", "child", "sibling"] });
+                                if (clickable) {
+                                    clickable.onDoubleClick = clickable.onClick;
+                                    delete clickable.onClick;
+                                } else {
+                                    Logger.warn("Could not find clickable element for channel", channel);
+                                }
+                            }
+
+                            return value;
+                        });
+
+                        Patcher.after(...ChatMessage, (_this, [{ iconType }], value) => {
+                            if (iconType !== "voice") return;
+                            const clickable = findInTree(value, (item) => item?.onClick && item.role === "link", { walkable: ["props", "children", "child", "sibling"] });
                             if (clickable) {
                                 clickable.onDoubleClick = clickable.onClick;
                                 delete clickable.onClick;
+                            } else {
+                                Logger.warn("Could not find clickable element for channel mention", value);
                             }
-                        }
-                    } else {
-                        Logger.warn('Failed to find channel.');
+                        });
                     }
-        
-                    return value;
-                });
 
-                Patcher.after(ChannelMention, "default", (that, args, value) => {
-                    const label = this.getLabel(value);
-                    if (label && label === 'Voice Channel') {
-                        const { props } = value;
-                        props.onDoubleClick = props.onClick;
-                        delete props.onClick;
+                    onStop() {
+                        Patcher.unpatchAll();
                     }
-        
-                    return value;
-                });
-            }
-
-			onStop() {
-                Patcher.unpatchAll();
-			}
-        
-            shouldDescend(key) {
-                return key === 'props' || key === 'children' || !isNaN(key);
-            }
-        
-            getChannel(obj) {
-                for (const key in obj) {
-                    const inner = obj[key];
-                    if (inner && typeof inner === 'object') {
-                        if (key === 'channel') {
-                            return inner;
-                        } else if (this.shouldDescend(key)) {
-                            return this.getChannel(inner);
-                        }
-                    }
-                }
-                return null;
-            }
-        
-            getClickable(obj) {
-                for (const key in obj) {
-                    const inner = obj[key];
-                    if (inner && typeof inner === 'object') {
-                        if (inner.onClick && inner.role === 'button') {
-                            return inner;
-                        } else if (this.shouldDescend(key)) {
-                            return this.getClickable(inner);
-                        }
-                    }
-                }
-                return null;
-            }
-        
-            getLabel(obj) {
-                for (const key in obj) {
-                    const inner = obj[key];
-                    if (inner) {
-                        if (key === 'aria-label') {
-                            return inner;
-                        } else if (typeof inner === 'object' && this.shouldDescend(key)) {
-                            return this.getLabel(inner);
-                        }
-                    }
-                }
-                return null;
-            }
-        };
-    };
-    return plugin(Plugin, Api);
-})(global.ZeresPluginLibrary.buildPlugin(config));
+                };
+            };
+            return plugin(Plugin, Api);
+        })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
 /*@end@*/
